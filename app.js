6a25664fd7b3f4a3520a54509cef9ed737a59286
tchandjohokader <tchandjohokadercoulibaly@gmail.com>
2023-09-25 00:22:56 +0000

    Update

--- app.js
+++ app.js
@@ -1,32 +1,46 @@
+require('dotenv').config({path:'./src/config/.env'})
 const express =require('express')
 const app =express()
 const uuid =require('uuid');
 const date = require('date-and-time');
 const redirectify = require('redirectify');
 const bodyParser = express.json;
-const mysql =require('mysql')
+const mysql =require('mysql');
 const mysqlconnection =require('express-myconnection');
-require('dotenv').config({path:'./src/config/.env'})
-const db=require('./src/config/db')
+const db=require('./src/config/db');
+const session  =  require ( 'express-session' );
 const cors = require('cors');
+//const bcrypt = require('bcrypt');
 let PORT=process.env.PORT || 5000
 
-app.use(bodyParser())
+app.use(express.json());
 app.use(cors())
-app.use(mysqlconnection(mysql,db.donnedeconnection,'pool'))
+app.use(mysqlconnection(db.Tbase,db.donnedeconnection,'pool'))
 app.use(express.urlencoded({extended:false}))
+//connection compte
+app.use(session({
+    name:process.env.SESSION_NAME,
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { 
+        maxAge:1000*60*60*24,
+        secure: false
+    }
 
+  }))
 //recuperation db
 app.get('/',(req,res)=>{
+    //console.log(req.session.iduser)
     req.getConnection((error,connection)=>{
         if(error){
-            res.status(404).send('erreur de connection')
+            res.status(404).json('erreur de connection')
         }else{
             connection.query('SELECT * FROM utilisateur',(error,resultat)=>{
                 if(error){
-                    res.status(404).send('erreur de commande')
+                    res.status(404).json('erreur de commande')
                 }else{
-                    res.send(resultat)
+                    res.json(resultat)
                 }
             })
         }
@@ -34,10 +48,8 @@ app.get('/',(req,res)=>{
 })
 
 //insertion db
-
-
 app.post('/inscription',(req,res)=>{
-    let id =uuid.v4()='pmp'
+    let id =uuid.v4()
     let nom=req.body.nom
     let prenom=req.body.prenom
     let email=req.body.email
@@ -49,44 +61,43 @@ app.post('/inscription',(req,res)=>{
             const query = `INSERT INTO utilisateur(id_user, Nom, Prenom, Email, Motdepasse) VALUES (?, ?, ?, ?, ?)`;
             connection.query(query,[id,nom,prenom,email,motdepasse],(error,resultat)=>{
                 if(error){
-                    res.status(200).json(error)
+                    res.status(400).json(error.message)
                     //id =uuid.v4()
                 }else{
-                    //res.redirect('/')
+                    res.status(200).json('inscription')
                 }
             })
         }
     })
 })
 //connection
-app.post('/connection',(req,res)=>{
+app.post('/connection', (req,res)=>{
     let email=req.body.email
-    let motdepasse=req.body.motdepasse
+    let motdepasse = req.body.motdepasse
     req.getConnection((error,connection)=>{
         if (error) {
-            res.status(404).send('erreur de connection avec la db')
+            res.status(404).json('erreur de connection avec la db')
         }else{
-            connection.query("SELECT * FROM utilisateur WHERE Email = ? AND Motdepasse=? ",[email,motdepasse],(error,resultat)=>{
+            connection.query("SELECT * FROM utilisateur WHERE Email = ? AND Motdepasse =? ",[email,motdepasse],(error,resultat)=>{
                 if(error){
-                    res.status(404).send('erreur de commande')
+                    res.status(404).json('erreur de commande')
                 }else{
-                    resultat.length ===0? res.status(400).send('email ou mot de passe incorrecte'):res.status(200).send('connection')
-                    console.log(resultat.length)
+                    resultat.length ===0? res.status(400).json('email ou mot de passe incorrecte'): req.session.iduser=resultat[0].id_user;res.status(200).json('connection')
                 }
             })
         }
     })
 })
 //afficher une seul ong
-app.get('/faireundon/:id',(req,res)=>{
+app.get('/faireundon/:nom_ong',(req,res)=>{
     let id=req.params.id
     req.getConnection((error,connection)=>{
         if (error) {
-            res.status(404).send('erreur de connection avec la db')
+            res.status(404).json('erreur de connection avec la db')
         }else{
             connection.query("SELECT * FROM ong WHERE id_ong = ? ",[id],(error,resultat)=>{
                 if(error){
-                    res.status(404).send('erreur de commande')
+                    res.status(404).json('erreur de commande')
                 }else{
                    res.status(201).json(resultat)
                 }
@@ -99,11 +110,11 @@ app.get('/recherche/:lettre',(req,res)=>{
     let lettre=req.params.lettre
     req.getConnection((error,connection)=>{
         if (error) {
-            res.status(404).send('erreur de connection avec la db')
+            res.status(404).json('erreur de connection avec la db')
         }else{
             connection.query(`SELECT * FROM ong WHERE Nom_ong LIKE '%${lettre}%'`,(error,resultat)=>{
                 if(error){
-                    res.status(404).send('erreur de commande')
+                    res.status(404).json('erreur de commande')
                 }else{
                    res.status(201).json(resultat)
                 }
@@ -116,11 +127,11 @@ app.get('/ong/:categorie',(req,res)=>{
     let categorie=req.params.categorie
     req.getConnection((error,connection)=>{
         if (error) {
-            res.status(404).send('erreur de connection avec la db')
+            res.status(404).json('erreur de connection avec la db')
         }else{
             connection.query(`SELECT * FROM ong WHERE Domaine_ong =?`,[categorie],(error,resultat)=>{
                 if(error){
-                    res.status(404).send('erreur de commande')
+                    res.status(404).json('erreur de commande')
                 }else{
                    res.status(201).json(resultat)
                 }
@@ -129,18 +140,18 @@ app.get('/ong/:categorie',(req,res)=>{
     })
 })
 //faire un don materiel
-app.post('/don/materiel/:id',(req,res)=>{
+app.post('/don/materiel/:nom_ong',(req,res)=>{
     let id_ong =req.params.id
     let categorie=req.body.categorie
     let query = "INSERT INTO aide(id_user, id_ong, type_aide, categorie, date_aide) VALUES (?, ?, ?, ?, ?)";
     req.getConnection((error,connection)=>{
         if(error){
-            res.status(500).send('erreur de connection avec la db')
+            res.status(500).json('erreur de connection avec la db')
         }else{
             
             connection.query(query,[id,id_ong,'materiel',categorie,date.format(new Date(), 'DD-MM-YYYY')],(error,resultat)=>{
                 if(error){
-                    res.status(404).send('error')
+                    res.status(404).json('error')
                 }else{
                     res.status(200).json('merci pour votre don')
                 }
@@ -150,18 +161,18 @@ app.post('/don/materiel/:id',(req,res)=>{
 })
 
 //faire un don en argent
-app.post('/don/argent/:id',(req,res)=>{
+app.post('/don/argent/:nom_ong',(req,res)=>{
     let id_ong =req.params.id
     let montant=req.body.montant
     let query = "INSERT INTO aide(id_user, id_ong, type_aide, montant, date_aide) VALUES (?, ?, ?, ?, ?)";
     req.getConnection((error,connection)=>{
         if(error){
-            res.status(500).send('erreur de connection avec la db')
+            res.status(500).json('erreur de connection avec la db')
         }else{
             
             connection.query(query,[id,id_ong,'financier',montant,date.format(new Date(), 'DD-MM-YYYY')],(error,resultat)=>{
                 if(error){
-                    res.status(404).send('error')
+                    res.status(404).json('error')
                 }else{
                     res.status(200).json('merci pour votre don')
                 }
@@ -172,19 +183,30 @@ app.post('/don/argent/:id',(req,res)=>{
 
 //Mon compte
 app.get('/moncompte',(req,res)=>{
-    let id_user=req.body.id_user
     req.getConnection((error,connection)=>{
         if(error){
             res.status(500).json('erreur de connection')
         }else{
-            connection.query('SELECT * FROM aider WHERE id_user = ?',[id_user],(error,resultat)=>{
-                if(error){
-                    res.status(500).json('commande invalide')
-                }else{
-                    res.status(200).json(resultat)
-                }
-            })
-        }
+            if(req.session.iduser){
+                connection.query('SELECT date_aide,montant,type_aide,Nom_ong FROM aider WHERE id_user = ?',[req.session.iduser],(error,resultat)=>{
+                    if(error){
+                        res.status(500).json('commande invalide')
+                    }else{
+                        res.status(200).json(resultat)
+                    }
+                })}
+            else{
+                res.status(400).json('pas de connection en cours')
+            }
+
+            }
+    })
+})
+//deconnection
+app.get('/deconnection',(req,res)=>{
+    req.session.destroy((err)=>{
+        res.clearCookie(process.env.SESSION_NAME)
+        res.json('deconnecter')
     })
 })
 
--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,14 @@
+const Tbase = require('mysql');
 let host = process.env.HOST;
-let user=process.env.USER
-let password=process.env.PASSWORD
-let database=process.env.DATABASE
-const donnedeconnection ={
-    host     : host,
-    user     : user,
-    password : password,
-    database : database
+let user = process.env.USER;
+let password = process.env.PASSWORD;
+let database = process.env.DATABASE;
+
+const donnedeconnection = {
+    host: host,
+    user: user,
+    password: password,
+    database: database
 }
 
-module.exports.donnedeconnection=donnedeconnection
+module.exports = { donnedeconnection, Tbase };
