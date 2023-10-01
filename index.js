8f293e85e4540ea667fe5a51e9dabae7ecfee8c3
tchandjohokader <tchandjohokadercoulibaly@gmail.com>
2023-10-01 19:23:30 +0000

    update

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const e = require('cors');
 const puppeteer = require('puppeteer');
-
 (async () => {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
@@ -13,15 +11,14 @@ const puppeteer = require('puppeteer');
         })
         return e})
         em=e.splice(1,3);
-        console.log(em)
+        //console.log(em)
         for(const e of em){
         const selector=`#${e} > div.flex.w-full > div > div:nth-child(1) > div.flex-1 > h2 > a`
-        //await page.waitForNavigation();
         await page.waitForSelector(selector);
         await page.click(selector)
         const z=await page.evaluate(()=>{
             let el=[]
-            console.log('selector')
+            el = typeof el == 'undefined'? el : [];
             let description =document.querySelector('#short-description');
             let nom = document.querySelector("#goafrica-main-container > div > div.boxify.rounded-none.t\\:rounded-lg.border-l-0.border-r-0.t\\:border-l.t\\:border-r.mt-\\[80px\\] > div > div.px-10.mt-8.flex.flex-col.flex-wrap > div > div.w-full.t\\:w-auto.t\\:flex-1.mb-8.flex.flex-col.gap-6 > h1");
             let numero = document.querySelector("#goafrica-main-container > div > div.w-full.flex.flex-wrap.mt-g > div.w-full.ls\\:w-7\\/12.ls\\:pr-g.flex.flex-col.gap-y-g > div:nth-child(2) > div > div > div.flex.flex-wrap.ls\\:flex-nowrap.text-gray-700.ls\\:gx-8 > div.flex.ls\\:block.flex-wrap.justify-between.w-full.ls\\:w-1\\/2.mt-12.ls\\:mt-0 > div > div > div > a");
@@ -36,9 +33,7 @@ const puppeteer = require('puppeteer');
             return el
         })
         console.log(z)
-        setTimeout(async()=>{
-            await page.goBack();
-        },3000)
+        await page.goBack();
         }
 })();
 
